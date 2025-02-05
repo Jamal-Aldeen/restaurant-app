03e5100af3a20b92b6c03e96c7966f51b2c54a6c
Essam-sara <saraessam2580@gmail.com>
2025-02-05 21:48:53 +0200

    create login logic

---
+++ login.php
@@ -0,0 +1,38 @@
+<?php
+include('config/database.php');
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+   
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+
+    if (empty($email) || empty($password)) {
+        $error = "Please fill in all fields.";
+    } else {
+        
+        $stmt = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+        $stmt->execute(['email' => $email]);
+        $user = $stmt->fetch();
+
+        if ($user && password_verify($password, $user['password'])) {
+           
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['email'] = $user['email'];
+            $_SESSION['role'] = $user['role'];
+
+            if ($user['role'] == 'staff') {
+                header('Location: admin/dashboard.php');
+            } else {
+                header('Location: customer/dashboard.php');
+            }
+            exit;
+        } else {
+            $error = "Invalid email or password";
+        }
+    }
+}
+?>
+
+
